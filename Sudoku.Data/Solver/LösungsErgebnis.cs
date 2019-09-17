7413b054bb1b4ab0b32aa7dc3680c158a72cc290
Zakant <zakant@gmx.de>
2019-09-17 13:01:48 +0200

    Added base files for solver.

---
+++ Sudoku.Data/Solver/LösungsErgebnis.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku.Data.Solver
+{
+    public class LösungsErgebnis
+    {
+        public LösungsStatus Status { get; protected set; }
+
+        public SudokuFeld Feld { get; protected set; }
+
+        public LösungsErgebnis(LösungsStatus status) : this(status, null) { }
+
+        public LösungsErgebnis(LösungsStatus status, SudokuFeld feld)
+        {
+            Status = status;
+            if (Status == LösungsStatus.Erfolgreich && feld == null)
+                throw new ArgumentNullException("Status ist erfolgreich, aber feld ist null!");
+            Feld = feld;
+        }
+    }
+}
---
+++ Sudoku.Data/Solver/SudokuLöser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku.Data.Solver
+{
+
+    public static class SudokuLöser
+    {
+        public static LösungsErgebnis Lösen(this SudokuFeld feld)
+        {
+            
+        }
+
+        public static Task<LösungsErgebnis> LösenAsync(this SudokuFeld feld)
+        {
+            return Task.Run(() => Lösen(feld));
+        }
+    }
+}
