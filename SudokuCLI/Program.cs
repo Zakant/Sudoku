66d568176746a6310dd79034d0859fb7e41563f5
Zakant <zakant@gmx.de>
2019-09-18 11:16:16 +0200

    Added some test code.

--- SudokuCLI/Program.cs
+++ SudokuCLI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Sudoku.Data.Helper;
+using Sudoku.Data.Solver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,22 @@ namespace SudokuCLI
     {
         static void Main(string[] args)
         {
+            string sudokuString = @"530070000
+                                    600195000
+                                    098000060
+                                    800060003
+                                    400803001
+                                    700020006
+                                    060000280
+                                    000419005
+                                    000080079";
+            var sudoku = LadeHelper.LadeVonText(sudokuString);
+            Console.WriteLine(sudoku.ToString());
+            Console.WriteLine("\nSolving...");
+            var result = SudokuLöser.Lösen(sudoku);
+            Console.WriteLine("Solution:");
+            Console.WriteLine(result.Feld);
+            Console.ReadLine();
         }
     }
 }
